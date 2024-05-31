a39bb96bea0c5ece953bb0b74f4f99b926f8fb65
Matteo0421 <matteo.carola99@gmail.com>
2024-05-31 16:05:11 +0200

    [add]list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
+
   export default {
     
+
+    data(){
+      return{
+
+        projects: [],
+
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+        .then(result =>{
+          console.log(result.data);
+          this.projects = result.data
+        })
+        .catch(error=>{
+          console.log(error.message);
+        })
+
+      }
+    },
+
+    mounted(){
+      this.getApi();
+    }
+
   }
 </script>
 
 <template>
   <div>
-    prova
+    <h2>Lista Progetti</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+         {{ project.id }} - {{ project.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
