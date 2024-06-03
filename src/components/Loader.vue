437877714736df328de16edefc29155a977cc035
Matteo0421 <matteo.carola99@gmail.com>
2024-06-03 16:15:32 +0200

    [add]loading

---
+++ src/components/Loader.vue
@@ -0,0 +1,38 @@
+<script>
+  export default {
+    
+  }
+</script>
+
+<template>
+  <div class="loader"></div>
+</template>
+
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+
+/* HTML: <div class="loader"></div> */
+.loader {
+  width: fit-content;
+  font-weight: bold;
+  font-family: monospace;
+  font-size: 30px;
+  background:linear-gradient(90deg,#000 50%,#0000 0) right/200% 100%;
+  animation: l21 2s infinite linear;
+}
+.loader::before {
+  content :"Loading...";
+  color: #0000;
+  padding: 0 5px;
+  background: inherit;
+  background-image: linear-gradient(90deg,#fff 50%,#000 0);
+  -webkit-background-clip:text;
+          background-clip:text;
+}
+
+@keyframes l21{
+  100%{background-position: left}
+}
+
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -2,57 +2,102 @@
 import axios from 'axios';
 import { store } from '@/data/store';
 import Card from '@/components/Card.vue';
+import Loader from '@/components/Loader.vue'
+
 export default {
   components: {
     Card,
+    Loader // Aggiungi il componente Loader
   },
   data() {
     return {
-
       projects: [],
-
+      currentPage: 1,
+      lastPage: 1,
+      isLoading: false
     }
   },
-
   methods: {
-    getApi() {
-      axios.get(store.apiUrl)
+    getApi(page = 1) {
+      this.isLoading = true;
+      axios.get(`${store.apiUrl}?page=${page}`)
         .then(result => {
           console.log(result.data);
-          this.projects = result.data
+          this.projects = result.data;
+          this.currentPage = result.data.current_page;
+          this.lastPage = result.data.last_page;
         })
         .catch(error => {
           console.log(error.message);
         })
-
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    nextPage() {
+      if (!this.isLoading && this.currentPage < this.lastPage) {
+        this.getApi(this.currentPage + 1);
+      }
+    },
+    prevPage() {
+      if (!this.isLoading && this.currentPage > 1) {
+        this.getApi(this.currentPage - 1);
+      }
+    },
+    goToPage(page) {
+      if (!this.isLoading) {
+        this.getApi(page);
+      }
     }
   },
-
   mounted() {
     this.getApi();
   }
-
 }
 </script>
+
+
 
 <template>
   <div>
     <section class="py-5">
       <div class="container px-5 mb-5">
         <div class="text-center mb-5">
-          <h1 class="display-5 fw-bolder mb-0"><span class=" d-inline">Projects</span></h1>
+          <h1 class="display-5 fw-bolder mb-0"><span class="d-inline">Projects</span></h1>
+          <!-- Loader -->
+          <Loader v-if="isLoading" />
         </div>
-        <div class="row gx-5 justify-content-center">
+        <div v-if="!isLoading" class="row gx-5 justify-content-center">
           <div class="col-lg-11 col-xl-9 col-xxl-8">
-            <!-- Project Card 1-->
-            <Card v-for=" project in projects" :key="project.id" :title="project.title" :image="project.image"
-              :description="project.description" />
-
+            <Card
+              v-for="project in projects.data"
+              :key="project.id"
+              :title="project.title"
+              :image="project.image"
+              :description="project.description"
+            />
           </div>
         </div>
+        <nav v-if="!isLoading" aria-label="Page navigation">
+          <ul class="pagination justify-content-center">
+            <li class="page-item" :class="{ disabled: currentPage === 1 }">
+              <a class="page-link" href="#" @click.prevent="prevPage">Previous</a>
+            </li>
+            <li
+              v-for="page in lastPage"
+              :key="page"
+              class="page-item"
+              :class="{ active: currentPage === page }"
+            >
+              <a class="page-link" href="#" @click.prevent="goToPage(page)">{{ page }}</a>
+            </li>
+            <li class="page-item" :class="{ disabled: currentPage === lastPage }">
+              <a class="page-link" href="#" @click.prevent="nextPage">Next</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </section>
-
   </div>
 </template>
 
